9341b160bb018899857585faedd8973e2fe7c794
Pranav Katiyar <pranav.katiyar@decimal.co.in>
2023-03-31 19:52:49 +0530

    dynamic svg handling

--- components/home.js
+++ components/home.js
@@ -1,8 +1,15 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import {SvgXml} from 'react-native-svg'
+import React,{useState} from 'react';
 
 const Home = () => {
-    return (
+  // const [arrowColor, setColor] = useState('blue');
+  // const arrowSvg = `<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20" fill="${arrowColor}"><path d="M266 961q-23-23-23-56t23-56l273-273-274-274q-23-24-23.5-55.5T265 190q23-22 56.5-22t55.5 22l337 336q11 11 16.5 24t5.5 26q0 13-5.5 25.5T714 625L377 962q-25 25-56 23.5T266 961Z"/></svg>`;
 
+  // setTimeout(() => {
+  //   setColor('red');
+  // }, 2000);
+    return (
         <View style={styles.container}>
             <View style={styles.upperContainer}>
                 <Text style={styles.header}>Schedule Master</Text>
@@ -13,15 +20,15 @@ const Home = () => {
                 <TouchableOpacity style={styles.courses} >
                     <Text style={styles.text1}>Courses</Text>
                 </TouchableOpacity>
-    
                 <TouchableOpacity style={styles.courses} >
                     <Text style={styles.text1}> Faculties</Text>
                 </TouchableOpacity>
-    
                 <TouchableOpacity style={styles.courses} >
                     <Text style={styles.text1}>Schedules</Text>
                 </TouchableOpacity>
-    
+          {/* <View>
+            <SvgXml xml={arrowSvg} height={30} width={30}></SvgXml>
+                </View> */}
             </View>
             <Image style={styles.picture1} source={require('.././assets/photos/footer.png')} />
         </View>
@@ -36,8 +43,6 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'space-between',
-  
-  
     },
     picture2: {
       height: 200,
@@ -63,7 +68,6 @@ const styles = StyleSheet.create({
       marginLeft: '42%',
     },
     lowerContainer: {
-  
       justifyContent: 'flex-end',
     },
     courses: {
