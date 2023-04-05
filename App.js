d107988d58d25a935686b25323a6fd4674e12aeb
Apeksha16 <apeksha16verma@gmail.com>
2023-04-05 16:39:52 +0530

    4th & 5th screen's UI fixes

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import Login from './components/login';
 import Otp from './components/otp';
 import Home from './components/home';
 import CourseList from './components/courses';
-import BcaStructure from './components/bcaStructure';
+import CourseStructure from './components/courseStructure';
 
 const Stack = createNativeStackNavigator();
 
@@ -22,7 +22,7 @@ const App = () => {
           <Stack.Screen name="Verify OTP" component={Otp} />
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Courses" component={CourseList} />
-          <Stack.Screen name="Course Structure" component={BcaStructure} />
+          <Stack.Screen name="Course Structure" component={CourseStructure} />
         </Stack.Navigator>
       </NavigationContainer>
     </View>
